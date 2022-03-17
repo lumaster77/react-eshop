58f6281599b727c6ae64905245551c3e7bd538c1
jose guadalupe luquin angel <jglajose54@hotmail.com>
2022-03-17 02:00:21 -0600

    uso de hook y useEffect y useStatus con react y consumo de api

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -3,24 +3,23 @@ import '@styles/ProductItem.scss';
 
 import addtoCart from '@icons/bt_add_to_cart.svg';
 
-const ProductItem = () => {
-	const [cart, setCart] = useState('');
+const ProductItem = ({product}) => {
+	const [cart, setCart] = useState([]);
 
 	const handleClick = () =>{
-		setCart('Hola Mundo')
+		setCart([]);
 	}
     return (
 		<div className="ProductItem">
-			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+			<img src={product.images[0]} alt={product.title} />
 			<div className="product-info">
 				<div>
-					<p>$120,00</p>
-					<p>Bike</p>
+					<p>${product.price}</p>
+					<p>{product.title}</p>
 				</div>
 				<figure onClick={handleClick}>
 					<img src={addtoCart} alt="" />
 				</figure>
-				{cart}
 			</div>
 		</div>
     );
